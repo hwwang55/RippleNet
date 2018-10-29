ba7ce72f12b101575925d022ccdbab4b4b4cf91a
hwwang55 <wanghongwei55@gmail.com>
2018-10-29 11:27:34 +0800

    update default settings

--- src/main.py
+++ src/main.py
@@ -7,7 +7,7 @@ np.random.seed(555)
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--dataset', type=str, default='movie', help='which dataset to use')
-parser.add_argument('--dim', type=int, default=4, help='dimension of entity and relation embeddings')
+parser.add_argument('--dim', type=int, default=16, help='dimension of entity and relation embeddings')
 parser.add_argument('--n_hop', type=int, default=2, help='maximum hops')
 parser.add_argument('--kge_weight', type=float, default=0.01, help='weight of the KGE term')
 parser.add_argument('--l2_weight', type=float, default=1e-7, help='weight of the l2 regularization term')
@@ -20,12 +20,12 @@ parser.add_argument('--item_update_mode', type=str, default='plus_transform',
 parser.add_argument('--using_all_hops', type=bool, default=True,
                     help='whether using outputs of all hops or just the last hop when making prediction')
 
-# default settings for Book-Crossing
 '''
+# default settings for Book-Crossing
 parser = argparse.ArgumentParser()
 parser.add_argument('--dataset', type=str, default='book', help='which dataset to use')
 parser.add_argument('--dim', type=int, default=4, help='dimension of entity and relation embeddings')
-parser.add_argument('--n_hop', type=int, default=3, help='maximum hops')
+parser.add_argument('--n_hop', type=int, default=2, help='maximum hops')
 parser.add_argument('--kge_weight', type=float, default=1e-2, help='weight of the KGE term')
 parser.add_argument('--l2_weight', type=float, default=1e-5, help='weight of the l2 regularization term')
 parser.add_argument('--lr', type=float, default=1e-3, help='learning rate')
